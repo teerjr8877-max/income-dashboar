777977934d9bedcfeccc91b42bce82c67920d885
teerjr8877-max <teerjr8877@gmail.com>
2026-03-23 21:55:29 -0500

    Fix CSS loading as module in static build

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { React, createRoot, html } from './lib/react.js'
 import App from './App.jsx'
 import { ErrorBoundary } from './components/ErrorBoundary.jsx'
-import './index.css'
 
 createRoot(document.getElementById('root')).render(
   html`<${React.StrictMode}><${ErrorBoundary}><${App} /></${ErrorBoundary}></${React.StrictMode}>`,
